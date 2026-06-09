76c268e5d0e6a477609ab808ccfb189c12cdbaac
AndrewCook <andrewmcook05@gmail.com>
2026-06-09 13:13:42 +0900

    Added Slider

---
+++ src/Components/ImageCarousel.tsx
@@ -0,0 +1,38 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+type ImageCarouselProps = {
+  images: string[];
+};
+
+export default function ImageCarousel({
+  images,
+}: ImageCarouselProps) {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      loop
+      className="w-full h-[500px]"
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <img
+            src={image}
+            alt={`Slide ${index + 1}`}
+            className="h-full w-full object-cover"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
